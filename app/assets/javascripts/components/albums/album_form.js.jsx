ad4c72774243d10ffe50a119831dbbf520eeacb7
John <johnsnyder7@gmail.com>
2015-11-18 18:12:04 -0500

    hard code current user.

--- app/assets/javascripts/components/albums/album_form.js.jsx
+++ app/assets/javascripts/components/albums/album_form.js.jsx
@@ -16,7 +16,8 @@ var AlbumForm = React.createClass({
     var album = {
       title: this.state.title,
       description: this.state.description,
-      user_id: CurrentUserStore.currentUserId()
+      user_id: 2
+      // temporary until we learn how to get current user
     };
 
     ApiUtil.createAlbum(album, function (id) {
