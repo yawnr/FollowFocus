27d83c4b87b9a873f11b229d6cbcc51843695fe1
John <johnsnyder7@gmail.com>
2015-11-18 00:11:01 -0500

    Search bar.

--- app/assets/javascripts/components/navbar.js.jsx
+++ app/assets/javascripts/components/navbar.js.jsx
@@ -13,9 +13,15 @@ var NavBar = React.createClass({
             </img>
           </ Link>
 
-        <li><Link to="/" className="link">Home</Link></li>
+        <li><Link to="/" className="link to-home">Home</Link></li>
         <li><Link to="/user/:userId" className="link">You</Link></li>
         <li><Link to="/explore" className="link">Explore</Link></li>
+        <div className="box">
+          <div className="container-2">
+              <span className="icon"><i className="fa fa-search"></i></span>
+              <input type="search" id="search" placeholder="Search for tags or users" />
+          </div>
+        </div>
         <Link to="/upload" className="upload-link">
           <img src={window.FollowFocus.images.uploadButton} alt="upload-button" className="upload-button" />
         </Link>
