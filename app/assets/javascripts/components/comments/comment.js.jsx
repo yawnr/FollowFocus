e7150c1db16131218bdb8b89bf10cf043bea0549
John <johnsnyder7@gmail.com>
2015-11-26 16:52:25 -0500

    dark map.

--- app/assets/javascripts/components/comments/comment.js.jsx
+++ app/assets/javascripts/components/comments/comment.js.jsx
@@ -3,8 +3,12 @@ var Comment = React.createClass({
   render: function () {
     return (
       <li className="comment group" key={this.props.comment.id}>
-        <div className="comment-user-pic" style={{backgroundImage: 'url(' + this.props.comment.user_photo + ')'}}></div>
-        <div className="comment-username">{this.props.comment.username}</div>
+        <a href={"#/users/" + this.props.comment.user_id}>
+          <div className="comment-user-pic" style={{backgroundImage: 'url(' + this.props.comment.user_photo + ')'}}></div>
+        </a>
+        <a href={"#/users/" + this.props.comment.user_id}>
+          <div className="comment-username">{this.props.comment.username}</div>
+        </a>
         <div className="comment-date">{this.props.comment.created_at}</div>
         <div className="comment-body">{this.props.comment.body}</div>
       </li>
