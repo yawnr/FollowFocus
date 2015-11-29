90ede57951a6d29264424573401da63d4333afab
John <johnsnyder7@gmail.com>
2015-11-28 22:38:24 -0500

    Fix camera roll view by randomizing tile size until exif size data is implemented.

--- app/assets/javascripts/components/photos/camera_roll_new.jsx
+++ app/assets/javascripts/components/photos/camera_roll_new.jsx
@@ -14,19 +14,21 @@ var CameraRollFreewall = React.createClass({
   },
 
   onChange: function () {
-    if (PhotosStore.all().length > 0) {
+    var photos = PhotosStore.all();
+
+    if (photos.length > 0) {
       var temp = "<a href='#/'><div class='cell' style='width:{width}px; height: {height}px; background-image: url(http://s3.amazonaws.com/FOLLOW-FOCUS-DEV/photos/photo_attachments/000/000/302/original/250352_587840231385_6942936_n.jpg?1448467066)'></div></a>";
-        var w = 1, html = '', limitItem = PhotosStore.all().length;
+        var w = 1, html = '', limitItem = photos.length;
         for (var i = 0; i < limitItem; ++i) {
 
-            var image = new Image();
-            image.src = PhotosStore.all()[i].small;
+            // var image = new Image();
+            // image.src = PhotosStore.all()[i].small;
+            //
+            var width = 200 + Math.random() * 200;
+            var height = 200 + Math.random() * 200;
 
-            var width = image.width;
-            var height = image.height;
-
-          w = 200 +  200 * Math.random() << 0;
-          html += temp.replace("#/", "#/albums/" + PhotosStore.all()[i].album_id + "/photos/" + PhotosStore.all()[i].id).replace(/\{height\}/g, height / 2).replace(/\{width\}/g, width / 2).replace("{index}", i + 1).replace("http://s3.amazonaws.com/FOLLOW-FOCUS-DEV/photos/photo_attachments/000/000/302/original/250352_587840231385_6942936_n.jpg?1448467066", PhotosStore.all()[i].small);
+          // w = 200 +  200 * Math.random() << 0;
+          html += temp.replace("#/", "#/albums/" + PhotosStore.all()[i].album_id + "/photos/" + PhotosStore.all()[i].id).replace(/\{height\}/g, height).replace(/\{width\}/g, width).replace("{index}", i + 1).replace("http://s3.amazonaws.com/FOLLOW-FOCUS-DEV/photos/photo_attachments/000/000/302/original/250352_587840231385_6942936_n.jpg?1448467066", PhotosStore.all()[i].small);
         }
         $("#freewall").html(html);
 
